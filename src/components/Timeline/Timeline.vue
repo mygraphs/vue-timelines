270bc915873f350ceb5d7ca974260b72835e9171
Sergio A. Martinez <smartinez@triditive.com>
2023-12-02 17:14:57 +0100

    [TIMELINE] Pre-work to adjust size
    Observe changes on the div so we can resize to fill all the
    space
    
    Signed-off-by: Sergio A. Martinez <smartinez@triditive.com>

--- src/components/Timeline/Timeline.vue
+++ src/components/Timeline/Timeline.vue
@@ -20,8 +20,24 @@ export default {
     calendarScrollToday: function () {
       this.$refs.timeline.scrollLeft = this.cellSize * (this.todayCell - 4);
     },
+    handleResize(width) {
+        console.log('New width:', width);
+    }
   },
   mounted() {
+
+    const observedElement = this.$refs.timeline;
+
+    const resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        const { width } = entry.contentRect;
+        // Call your callback function here with the new width
+        this.handleResize(width);
+      }
+    });
+
+    resizeObserver.observe(observedElement);
+
     this.$nextTick(() => {
       this.calendarScrollToday();
     });
