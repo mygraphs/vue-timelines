08bd107dee4d46717c823eb8e5c2222eee4526da
Sergio A. Martinez <smartinez@triditive.com>
2024-01-05 00:00:09 +0100

    [CLEANUP] Removed draggable dead code
    Signed-off-by: Sergio A. Martinez <smartinez@triditive.com>

--- src/components/Draggable/index.js
+++
@@ -1,5 +0,0 @@
-/*
-    Component from tutorial:
-    https://www.redblobgames.com/making-of/draggable/
-*/
-export { default as Draggable } from "./Draggable";
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,6 @@
-export * from "./Draggable";
 export * from "./Calendar";
 export * from "./TimelineHeader";
 export * from "./List";
 export * from "./Timeline";
 export * from "./TextEdit";
 export * from "./TaskDataPanel";
-
