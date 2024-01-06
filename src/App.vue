ad2febd0d2627d9d994680e0ef4948995d97cebc
Sergio A. Martinez <smartinez@triditive.com>
2024-01-06 15:12:42 +0100

    [Slider] Added slide bar to modify progress
    Signed-off-by: Sergio A. Martinez <smartinez@triditive.com>

--- src/App.vue
+++ src/App.vue
@@ -168,7 +168,7 @@ export default {
       }
 
       this.tasks[task.id] = task;
-      console.log("******* UPDATED TASK " + task.title + " *********8*** " + task.group_id);
+      console.log("******* UPDATED TASK " + task.title + " ********** " + task.group_id);
       //this.listTasks();
     },
   },
