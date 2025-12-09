b79ac1abe4fe5ce0990fee88d6f3f894891fabda
Sergio A. Martinez <sergio.martinez@riskpal.co.uk>
2025-12-09 08:04:22 +0000

    version: updated version
    Signed-off-by: Sergio A. Martinez <sergio.martinez@riskpal.co.uk>

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 // Version information for vue-timelines
 // This file is automatically updated during build or can be manually updated
-export const VERSION = '0.2.7';
+export const VERSION = '0.3.1';
 export const NAME = 'vue-timelines';
 export const DESCRIPTION = 'Vue timelines component';
 export const AUTHOR = 'mygraphs';
